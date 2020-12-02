35c615fa55dc9dea496aea8d125c1d2459e766d0
Artem Amirkhanov <artem.ogre@gmail.com>
2020-12-02 22:03:01 +0100

    #26 Remove envelopBox body from .hpp

--- CDT/include/CDTUtils.hpp
+++ CDT/include/CDTUtils.hpp
@@ -29,30 +29,6 @@ V2d<T> V2d<T>::make(const T x, const T y)
 //*****************************************************************************
 // Box2d
 //*****************************************************************************
-
-template <
-    typename T,
-    typename TVertexIter,
-    typename TGetVertexCoordX,
-    typename TGetVertexCoordY>
-Box2d<T> envelopBox(
-    TVertexIter first,
-    TVertexIter last,
-    TGetVertexCoordX getX,
-    TGetVertexCoordY getY)
-{
-    const T max = std::numeric_limits<T>::max();
-    Box2d<T> box = {{max, max}, {-max, -max}};
-    for(; first != last; ++first)
-    {
-        box.min.x = std::min(getX(*first), box.min.x);
-        box.max.x = std::max(getX(*first), box.max.x);
-        box.min.y = std::min(getY(*first), box.min.y);
-        box.max.y = std::max(getY(*first), box.max.y);
-    }
-    return box;
-}
-
 template <typename T>
 Box2d<T> envelopBox(const std::vector<V2d<T> >& vertices)
 {
